d802ba732593f85e351592fcaa810086c7415de6
HtinSharKyaw <htinsharrkyaw@gmail.com>
2022-07-10 00:12:30 -0500

    jsx control flow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ['john','doe','james'];
+    const random = Math.floor(Math.random() * 3) ;
+    return names[random]; 
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to see what i change.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>Hello {handleNameChange()}</p>
+      
+    </div> 
   );
 }
 
