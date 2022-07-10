42a3af8b4a43eb229da7ac1079dab2983eb46d4a
HtinSharKyaw <htinsharrkyaw@gmail.com>
2022-07-10 10:28:43 -0500

    event hadler added

--- src/Content.js
+++ src/Content.js
@@ -7,9 +7,24 @@ const Content = () => {
     return names[random]; 
   } 
   
+  const handleClick = () => {
+    console.log('you clicked it');
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`you clicked it ${name}`);
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText);
+  }
+
   return (
     <main>
-    <p>Hello {handleNameChange()}</p>
+    <p>Hello {handleNameChange()}!</p>
+    <button onClick={handleClick}>Click it</button>
+    <button onClick={() => handleClick2('John')}>Click it</button>
+    <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
   )
 }
